070c2f2dd7b7781c5f820ec172019664aaa14c2a
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-12 12:24:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
