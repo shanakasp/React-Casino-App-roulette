0b9aefa66e12ba6f8d2232f4fda39dcd8da3c0bc
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-13 22:00:57 +0530

    modified 3 files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import { Avatar, Typography } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
+      <div className="logo-container">
+        <Avatar></Avatar>
+        <Typography.Title level={2} className="logo">
+          <Link to="/">CryPrince</Link>
+        </Typography.Title>
+      </div>
       <h1>Navbar</h1>
     </div>
   );
